8095f32de47e7a7cc9ac37825981900ddf25c415
DK <kalluskydominik@cmu-206444.wv.cc.cmu.edu>
2011-03-20 20:35:53 -0400

    Set up basic structure

---
+++ SystemB/Events.java
@@ -0,0 +1,14 @@
+public class Events
+{
+	final public static String DOOR_BROKEN = "DB";
+	final public static String WINDOW_BROKEN = "WB";
+	final public static String MOTION_DETECTED = "MD";
+	final public static String ARM_SYSTEM = "AS";
+	final public static String DISARM_SYSTEM = "DAS";
+	final public static String FIRE_ALARM = "FA";
+	final public static String SPRINKLER_ON = "SO";
+	final public static String SPRINKLER_OFF = "SOF";
+	final public static String SPRINKLER_CONFIRM = "SC";
+	final public static String SPRINKLER_CANCEL = "SCA";
+	final public static String ALIVE = "A";
+}
---
+++ SystemC/EventIDs.java
@@ -0,0 +1,15 @@
+public class EventIDs
+{
+	final public static int DOOR_ID = 0;
+	final public static int WINDOW_ID = 1;
+	final public static int MOTION_ID = 2;
+	final public static int FIRE_ID = 3;
+	final public static int SPRINKLER_ID = 4;
+
+	final public static int SECURITY_MONITOR_ID= 5;
+	final public static int MAINTENANCE_MONITOR_ID= 6;
+	
+	final public static int SECURITY_CONTROLLER_ID=7;
+	final public static in FIREALARM_CONTROLLER_ID=8;
+	final public static int SPRINKLER_CONTROLLER_ID= 9;
+}
