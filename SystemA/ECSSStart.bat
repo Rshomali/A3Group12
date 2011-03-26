fe9b6c08f4c26476a871c83b1222d3c95afdfa30
taehyun <dana@.(none)>
2011-03-26 18:20:47 -0400

    test

---
+++ SystemA/ECSSStart.bat
@@ -0,0 +1,21 @@
+%ECHO OFF
+%ECHO Starting Security System
+PAUSE
+%ECHO Security Monitoring Console
+START "MUSEUM ENVIRONMENTAL CONTROL SYSTEM SECURITY CONSOLE" /NORMAL java SecurityConsole %1
+%ECHO Starting Security Controller Console
+START "SECURITY CONTROLLER CONSOLE" /MIN /NORMAL java SecurityController %1
+
+
+%ECHO Starting Door Sensor Console
+START "Door SENSOR CONSOLE" /MIN /NORMAL java DoorSensor %1
+
+%ECHO Starting Window Sensor Console
+START "WINDOW SENSOR CONSOLE" /MIN /NORMAL java WindowSensor %1
+
+%ECHO Starting Motion Sensor Console
+START "Motion SENSOR CONSOLE" /MIN /NORMAL java MotionSensor %1
+
+%ECHO Starting Fire Sensor Console
+START "Fire SENSOR CONSOLE" /MIN /NORMAL java FireSensor %1
+
